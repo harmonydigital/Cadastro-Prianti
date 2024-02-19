79b0103ee547be5a4dd723292bf354d4a79fa222
harmonydigital <131012395+harmonydigital@users.noreply.github.com>
2024-02-19 11:36:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ const form = document.getElementById ("dataSubmit")
 let inputs=document.querySelectorAll("input")
 let msg=document.getElementById ("msg")
 let arrInputs=Array.from(inputs)
- 
+let jsonArr=[]
 
 
 document.querySelector('form').addEventListener('submit', (e)=>{
  e.preventDefault()
-   
-  alert('teste')
+    
+  arrInputs.map((mapInp)=>{ 
+        var name=mapInp.getAttribute('name')
+        jsonArr.push({
+        [mapInp.getAttribute('name')]:mapInp.value, 
+    }); 
+  })
   
+ console.log(jsonArr)
+  alert('data')
 })
 
-
- 
- 
-arrInputs.map((mapInp)=>{
-  console.log(mapInp.getAttribute('name'))
-})
