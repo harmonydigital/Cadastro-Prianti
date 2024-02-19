4b813d85fb829bd3a6d37bcab2414f400675f2f9
harmonydigital <131012395+harmonydigital@users.noreply.github.com>
2024-02-19 10:52:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,12 @@ const form = document.getElementById ("dataSubmit")
 let inputs=document.querySelectorAll("input")
 let msg=document.getElementById ("msg")
 
+ 
 
-document.querySelector('.submit').addEventListener("click", (e)=>{
+
+document.querySelector('form').addEventListener('submit', (e)=>{
  e.preventDefault()
- 
- 
-   alert('teste')
-  
-  
+   
+  alert('teste')
   
 })
-
-
-
